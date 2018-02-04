92729efbfc2298ecce8a27f9602be4dbeba0b2a3
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-02-04 01:31:16 +0100

    persist

--- stores/read.js
+++ stores/read.js
@@ -5,12 +5,22 @@ var fs = require('fs')
 var alice = fs.readFileSync(path.join(__dirname, '../assets/11.txt'), 'utf8')
 
 module.exports = function (state, emitter) {
+  state.read = {
+    index: 0
+  }
+
   emitter.on('DOMContentLoaded', () => {
+    var local = window.localStorage.index
+    state.read.index = local ? JSON.parse(local) : 0
+
     emitter.on('notification:granted', () => {
       var arr = alice.split('\n')
-      mapLimit(arr, 1, iterate, done)
+      emitter.emit('log:info', `index is ${state.read.index}`)
+      arr.splice(0, state.read.index)
+      mapLimit(arr, 1, iterate, () => console.log('done!'))
 
       function iterate (text, done) {
+        setState()
         if (!text) return done()
         emitter.emit('notification:new', text)
 
@@ -18,14 +28,15 @@ module.exports = function (state, emitter) {
           done()
         }, 3000)
       }
-
-      function done () {
-        console.log('done!')
-      }
     })
 
     setTimeout(() => {
       emitter.emit('notification:request')
     }, 3000)
   })
+
+  function setState () {
+    state.read.index += 1
+    window.localStorage.index = state.read.index
+  }
 }
